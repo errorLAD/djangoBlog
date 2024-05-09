350fdc7c6cc0cabf332aab9077147a80d0df1ba9
theabhishek <callabhimishra@gmail.com>
2024-05-09 15:00:51 +0530

    Update settings.py

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n8^@1$ll9wyo)$h5h+5xq-a7a!vnn&@5(gi#!3*9#t@=nqc&al
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "web-production-96b49.up.railway.app"]
 
 
 # Application definition
